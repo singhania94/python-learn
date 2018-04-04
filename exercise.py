4c21273a447d778b2a1c0851b9c94b325cab9f78
as66422 <aditya.ranglal.singhania@citi.com>
2018-04-04 19:27:19 +0530

    tutorial additions

---
+++ exercise.py
@@ -0,0 +1,14 @@
+
+print('oink oink')
+
+# REPL stands for Read evaluate Print Loop commands on console
+# Executes a file instead of interpreting
+
+myint = 9
+myfloat = 7.0
+myfloat2 = float(7)
+type(myfloat)       # prints type of variable
+myint**myfloat      # Exponentiation
+myint//myfloat2     # Integer division
+
+
