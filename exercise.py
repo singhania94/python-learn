071f832c980d3513c07e444ae945cea991bc98c2
as66422 <aditya.ranglal.singhania@citi.com>
2018-04-04 19:45:53 +0530

    tutorial-2

--- exercise.py
+++ exercise.py
@@ -1,14 +1,22 @@
-
-print('oink oink')
-
 # REPL stands for Read evaluate Print Loop commands on console
 # Executes a file instead of interpreting
 
-myint = 9
-myfloat = 7.0
-myfloat2 = float(7)
-type(myfloat)       # prints type of variable
-myint**myfloat      # Exponentiation
-myint//myfloat2     # Integer division
+var = 9
+var2 = 7.0
+var = float(7)
+type(var)           # prints type of variable
+var**var            # Exponentiation
+var = var//var2     # Integer division
+var = "tomorrow"
 
+# snake_case for variables
+# CAP_SNAKE_CASE for constants
+# UpperCamelCase for classes
+# __underscore__ no touch variables
+
+# bool, int, str, list, dict{JSON}
+var = True
+var = [2, 3, True, "wow"*3]*2
+
+print(var)
 
