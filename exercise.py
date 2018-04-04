186404d1a8a9b7cc02b979099789d9d44d4c4965
as66422 <aditya.ranglal.singhania@citi.com>
2018-04-04 20:39:43 +0530

    tutorial-3

--- exercise.py
+++ exercise.py
@@ -2,10 +2,13 @@
 # Executes a file instead of interpreting
 
 var = 9
+# print("Variable value is ", var, ", you idiot")
+# print(f"Variable value is {var * 89}, you idiot")
+# print("Variable value is {}".format(26))
 var2 = 7.0
 var = float(7)
 type(var)           # prints type of variable
-var**var            # Exponentiation
+var**var2           # Exponentiation
 var = var//var2     # Integer division
 var = "tomorrow"
 
@@ -17,6 +20,24 @@ var = "tomorrow"
 # bool, int, str, list, dict{JSON}
 var = True
 var = [2, 3, True, "wow"*3]*2
+var = None
+type(var)
+var = {
+    "name": "aditya",
+    "profession": ["tomorrow", 56],
+    "address": {
+        "plot": "New RadhaKisan plots"
+    }
+}
 
+# "" '' both can be used to declare strings
+var = " " + "tomorrow"
+var[2]
+var[-2]
+var[var.__len__()-1]
+var = str(9)
+# var = input()
+var = 7.74986824596
+round(var, 2)       # round 2 decimal points
 print(var)
 
