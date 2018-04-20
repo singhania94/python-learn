79308d2159cf5bc21838cf6f19edec96921d4b09
as66422 <aditya.ranglal.singhania@citi.com>
2018-04-20 19:04:53 +0530

    completed lambdas and inbuilt functions

--- syntax/oops_user.py
+++ syntax/oops_user.py
@@ -35,6 +35,7 @@ user1 = User()
 # User.print_class()
 # User.from_string("Aditya,Singhania").print_user()
 
+
 class Car(object):
     """
         blueprint for car
@@ -53,3 +54,45 @@ class Car(object):
 maruti_suzuki = Car("ertiga", "black", "suzuki", 60)
 audi = Car("A6", "red", "audi", 80)
 # audi.start()
+
+
+class Animal:
+
+    def __init__(self, name):
+        self._age = 0
+        self.name = name
+
+    @property
+    def age(self):              # allows us to call animal.age
+        return self._age
+
+    @age.setter
+    def age(self, value):       # allows us to set animal.age = 87, -78
+        self._age = max([0, value])
+
+    def make_sound(self, sound):
+        print(f'make sound {sound}')
+
+    def do_something(self):
+        raise NotImplementedError("Implement this method in child class")
+
+class Cat(Animal):
+    def __init__(self, name, species):
+        super().__init__(name)        # Animal.__init__(self, name)
+        self.species = species
+
+    def do_something(self):
+        pass
+
+# In multiple inheritance the first mentioned class is referred via super and both class
+# methods are available but first mentioned class superceeds authority
+
+# MRO method resolution order
+# first look in cat, then animal
+# __mro__ attribute, mro() method, help(class) provide a look into mro ordering
+
+# Polymorphism
+# same method, different classes - method override
+# same method, different inputs - method overload - mul(a, b); mul(a, b, c);
+# Dunder methods - __repr__, __str__, __add__(self, other), __len__, __mul__
+# in dictionary, __missing__, __setitem__, __iter__
